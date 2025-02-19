551af8ac0c65b954af680e87a16ff8f906b8ca5b
NareshKumar_getkart <naresh.kumar@getkart.com>
2025-02-19 16:45:15 +0530

    MErging coed

---
+++ Pods/Target Support Files/Pods-GetKart-GetKartUITests/Pods-GetKart-GetKartUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_GetKart_GetKartUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GetKart_GetKartUITestsVersionString[];
+
