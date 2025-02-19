b0238a8b712c83f2fe57306d9e7bf7cc5eed2e6c
Radhe@123 <radheshyam.getkart@gmail.com>
2025-02-19 16:01:01 +0530

    Merge code

---
+++ GetKart/ViewControllers/Chat/ChatListTblcell/ChatListTblCell.swift
@@ -0,0 +1,29 @@
+//
+//  ChatListTblCell.swift
+//  Getkart
+//
+//  Created by Radheshyam Yadav on 19/02/25.
+//
+
+import UIKit
+
+class ChatListTblCell: UITableViewCell {
+    
+    @IBOutlet weak var imgViewProfile:UIImageView!
+    @IBOutlet weak var lblName:UILabel!
+    @IBOutlet weak var lblDesc:UILabel!
+    @IBOutlet weak var bgView:UIView!
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ GetKart/ViewControllers/Profile/ProfileListTblCell.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileListTblCell.swift
+//  Getkart
+//
+//  Created by Radheshyam Yadav on 19/02/25.
+//
+
+import UIKit
+
+class ProfileListTblCell: UITableViewCell {
+
+    @IBOutlet weak var lblTitle:UILabel!
+    @IBOutlet weak var imgVwArrow:UIImageView!
+    @IBOutlet weak var imgVwIcon:UIImageView!
+    @IBOutlet weak var bgview:UIView!
+    @IBOutlet weak var btnSwitch:UISwitch!
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
